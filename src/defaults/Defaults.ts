52db6f1474ac8c678c557964dd56d492a4945615
Ed Knutson <knutsoned+github@gmail.com>
2024-07-10 12:36:16 -0500

    MOAR

--- src/defaults/Defaults.ts
+++ src/defaults/Defaults.ts
@@ -4,13 +4,13 @@ import { DirtSample } from 'src/defaults/DefaultSample';
 
 export class Defaults {
   public static samples = [
-    new DirtSample('36_TechNoir_ARP2600_C1.wav', 'Noir'),
+    new DirtSample('24_TechNoir_ARP2600_C0.wav', 'TechNoir'),
     new DirtSample('50_Marimba_DX100_D2.wav', 'Marimba'),
     new DirtSample('52 Funk Bass AELITA E2_0001.wav', 'Aelita'),
-    new DirtSample('M1 Organ Stab Vinyl Synths Fmaj9 C2', 'Vinyl'),
+    new DirtSample('M1 Organ Stab Vinyl Synths Fmaj9 C2.wav', 'Vinyl'),
     new DirtSample('55_BillyZaneBass_ARP2600_G2.wav', 'Zane'),
     new DirtSample('21_Sehnsucht_ARP2600_A-1.wav', 'Sehnsucht'),
-    new DirtSample('83 Acoustic Guitar Mirage B4.wav', 'Guitar'),
+    new DirtSample('95 Acoustic Guitar Mirage B5.wav', 'Guitar'),
     new DirtSample('Orbit Keys S950 C4.wav', 'Orbit'),
     new DirtSample('Stutter Bass Mirage C2.wav', 'Stutter'),
   ] as const;
