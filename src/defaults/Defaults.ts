b49016e4537d3fee1b20039478f16a5144be7013
Ed Knutson <knutsoned+github@gmail.com>
2024-07-10 12:28:25 -0500

    more samples

--- src/defaults/Defaults.ts
+++ src/defaults/Defaults.ts
@@ -4,13 +4,14 @@ import { DirtSample } from 'src/defaults/DefaultSample';
 
 export class Defaults {
   public static samples = [
-    new DirtSample('43_TechNoir_ARP2600_G1.wav', 'Noir'),
-    new DirtSample('Marimba DX100 C2.wav', 'Marimba'),
+    new DirtSample('36_TechNoir_ARP2600_C1.wav', 'Noir'),
+    new DirtSample('50_Marimba_DX100_D2.wav', 'Marimba'),
+    new DirtSample('52 Funk Bass AELITA E2_0001.wav', 'Aelita'),
+    new DirtSample('M1 Organ Stab Vinyl Synths Fmaj9 C2', 'Vinyl'),
     new DirtSample('55_BillyZaneBass_ARP2600_G2.wav', 'Zane'),
-    new DirtSample('Funk Bass AELITA C3.wav', 'Aelita'),
+    new DirtSample('21_Sehnsucht_ARP2600_A-1.wav', 'Sehnsucht'),
+    new DirtSample('83 Acoustic Guitar Mirage B4.wav', 'Guitar'),
     new DirtSample('Orbit Keys S950 C4.wav', 'Orbit'),
-    new DirtSample('19_Sehnsucht_ARP2600_G-1.wav', 'Sehnsucht'),
-    new DirtSample('Acoustic Guitar Mirage C6.wav', 'Guitar'),
     new DirtSample('Stutter Bass Mirage C2.wav', 'Stutter'),
   ] as const;
 }
