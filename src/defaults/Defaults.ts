4bc34926a44e169b2367b5ce4efd12303c301507
Ed Knutson <knutsoned+github@gmail.com>
2024-12-06 09:28:45 -0600

    add more technoir

--- src/defaults/Defaults.ts
+++ src/defaults/Defaults.ts
@@ -4,15 +4,13 @@ import { DirtSample } from 'src/defaults/DefaultSample';
 
 export class Defaults {
   public static samples = [
-    new DirtSample('24_TechNoir_ARP2600_C0.wav', 'TechNoir'),
-    new DirtSample('50_Marimba_DX100_D2.wav', 'Marimba'),
-    new DirtSample('52 Funk Bass AELITA E2_0001.wav', 'Aelita'),
-    new DirtSample('M1 Organ Stab Vinyl Synths Fmaj9 C2.wav', 'Vinyl'),
-    new DirtSample('55_BillyZaneBass_ARP2600_G2.wav', 'Zane'),
-    new DirtSample('21_Sehnsucht_ARP2600_A-1.wav', 'Sehnsucht'),
-    new DirtSample('95 Acoustic Guitar Chorus Mirage B5.wav', 'Guitar'),
-    new DirtSample('Orbit Keys S950 C4.wav', 'Orbit'),
-    new DirtSample('Stutter Bass Mirage C2.wav', 'Stutter'),
+    new DirtSample('24_TechNoir_ARP2600_C0.wav', 'C'),
+    new DirtSample('24_TechNoir_ARP2600_D0.wav', 'D'),
+    new DirtSample('24_TechNoir_ARP2600_E0.wav', 'E'),
+    new DirtSample('24_TechNoir_ARP2600_F0.wav', 'F'),
+    new DirtSample('24_TechNoir_ARP2600_G0.wav', 'G'),
+    new DirtSample('24_TechNoir_ARP2600_A0.wav', 'A'),
+    new DirtSample('24_TechNoir_ARP2600_B0.wav', 'B'),
   ] as const;
 }
 
